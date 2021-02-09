f0f173ef3dc86f313f1db854604092b0e6b4023c
Muhammad Usama <usamahabib1231@outlook.com>
2021-02-09 11:47:42 +0500

    Adding initial files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+     { 
+      // <Logo />
+      // <ImageLinkForm />
+      // <ImageRecognition />
+     }
+    </div>
+  );
+}
+
+export default App;
